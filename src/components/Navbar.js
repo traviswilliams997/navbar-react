c878e75c9062f793c577932a35798f57c4b01aba
Travis Williams <traviswilliams997@gmail.com>
2022-09-17 15:31:23 -0500

    Create responsive drop down menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,14 +11,15 @@ const handleClick = () => setClick(!click)
           <div className = 'logo'> 
             <img src = {Logo} alt='logo' />
           </div>
-          <ul className="nav-menu">
+          <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item"><a href="/">Home</a></li>
             <li className="nav-item"><a href="/">About</a></li>
             <li className="nav-item"><a href="/">Faq</a></li>
             <li className="nav-item"><a href="/">Contact</a></li>
           </ul>
-          <div className = 'hamburger'>
-              <FaBars  size={30} style = {{color: '#f8f8f8'}} />
+          <div className = 'hamburger' onClick={handleClick} >
+              {click ? (<FaTimes size={30} style = {{color: '#f8f8f8'}}/>) : (<FaBars  size={30} style = {{color: '#f8f8f8'}} />)}
+              
           </div>
         </div>
     )
