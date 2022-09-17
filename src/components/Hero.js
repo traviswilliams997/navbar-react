e632923881ecefdb15fc576604877064692c352f
Travis Williams <traviswilliams997@gmail.com>
2022-09-17 15:54:44 -0500

    Add information to body

---
+++ src/components/Hero.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { FaDatabase, FaAsterisk, FaAccusoft, FaFacebook, FaTwitter, FaGithub } from "react-icons/fa"
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className= "contianer">
+                <div className="content">
+                    <div className= "col-1">
+                        <h1> Data to enrich your</h1>
+                        <h1><span className="primary-color"> online business</span></h1>
+                        <p>
+                        orem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia
+                         quam eget neque laoreet dapibus. Sed id est eleifend, auctor ipsum at, 
+                         iaculis nisl. Etiam cursus enim ut neque consectetur porttitor.
+                        </p>
+                    
+                    <div className="used-by">
+                        <p>USED BY</p>
+                        <div className= "icons">
+                            <span><FaDatabase /> Staxx</span>
+                            <span><FaAsterisk /> Star AI</span>
+                            <span><FaAccusoft /> Accusoft</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col-2">
+                        <div className= "form-layout">
+                            <div className= 'form-container'>
+                                <p className="sign-in-txt">Sign in with</p>
+                                <div className="social-login">
+                                    <span><FaFacebook size={20} /></span>
+                                    <span><FaTwitter size={20} /></span>
+                                    <span><FaGithub size={20} /></span>
+                                </div>
+                                <div className="divider">
+                                    <p><span>Or</span></p>
+                                </div>
+                                <form action = ''>
+                                    <input type = 'text' placeholder="Name" />
+                                    <input type = 'text' placeholder="Email" />
+                                    <input type = 'text' placeholder="Password" />
+                                    <button>Create Your Account</button>
+                                </form>
+                            </div>
+                            <div className="form-footer">
+                                <p>
+                                  By signing up, you agree to our 
+                                  <span className="primary-color"> Terms, Data Policy</span>
+                                  and <span className="primary-color">Cookies Policy</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Hero
