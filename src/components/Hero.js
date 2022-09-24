ff6921b0e7e9df9fd9954229e8a2e3efe5d1a213
Travis Williams <traviswilliams997@gmail.com>
2022-09-24 17:33:52 -0500

    Edit hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
                         <h1> Data to enrich your</h1>
                         <h1><span className="primary-color"> online business</span></h1>
                         <p>
-                        orem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia
                          quam eget neque laoreet dapibus. Sed id est eleifend, auctor ipsum at, 
                          iaculis nisl. Etiam cursus enim ut neque consectetur porttitor.
                         </p>
